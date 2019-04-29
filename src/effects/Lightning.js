ed762ce5f869b37480fba32e269db7ba98d5abe6
Hadik <hadikcze@gmail.com>
2019-04-29 21:53:14 +0200

    Lightning assets

--- src/effects/Lightning.js
+++ src/effects/Lightning.js
@@ -1,14 +1,22 @@
 import Phaser from 'phaser';
 import Depths from 'structs/Depths';
 
-export default class Lightning extends Phaser.GameObjects.Image {
+export default class Lightning extends Phaser.GameObjects.Sprite {
     constructor (scene) {
-        super(scene, -1000, -1000, 'assets', 'lightning');
+        super(scene, -1000, -1000, 'assets2', 'light/light_1');
         this.scene.add.existing(this);
         this.setOrigin(0.5, 1);
         this.setDepth(Depths.ATTACKS);
         this.setActive(false);
         this.setVisible(false);
+        this.setScale(0.5);
+
+        this.animation = this.scene.anims.create({
+            key: 'lightning',
+            frames: this.scene.anims.generateFrameNames('assets2', { prefix: 'light/light_', end: 10, zeroPad: 0 }),
+            frameRate: 30,
+            hideOnComplete: true
+        });
     }
 
     /**
@@ -22,6 +30,8 @@ export default class Lightning extends Phaser.GameObjects.Image {
         this.setAlpha(1);
         this.setVisible(true);
         this.setActive(true);
+
+        this.play('lightning');
 
         this.scene.cameras.main.shake(80, 0.01);
         this.scene.time.addEvent({
